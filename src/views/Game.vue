880de5fd6b942c93be7be2a476e0b94f55860e3d
zmesede <zme.sede@gmail.com>
2025-11-04 10:37:17 +0100

    Develop (#16)
    * modification scss + money unlimited
    
    * ajout numérotation heure + légende courbe + visibilité tiles canvas
    
    * correction bug canvas
    
    ---------
    
    Co-authored-by: Zac <daicronoss@gmail.com>

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,8 +1,4 @@
-
-
-
-
-<script lang="ts">
+<script setup lang="ts">
 import Board from '../components/Board.vue'
 import BaseAlert from '../components/BaseAlert.vue'
 import EquipmentList from '../components/EquipmentList.vue'
@@ -11,6 +7,9 @@ import BoardConsumptionDetails from '../components/BoardConsumptionDetails.vue'
 import BoardConsumptionAddWindow from '../components/BoardConsumptionAddWindow.vue'
 import EnergyMenuAddEnergyWindow from '../components/EnergyMenuAddEnergyWindow.vue'
 import EnergyMenuUseEnergyWindow from '../components/EnergyMenuUseEnergyWindow.vue'
+</script>
+
+<script lang="ts">
 export default {
   name: 'Game',
   components: {
@@ -78,7 +77,6 @@ export default {
             :production-curve-props="gameParametersStore.getProductionCurve"
         />
       </div>
-
       <BoardConsumptionDetails
           :consumption-tile="boardStore.clickedTile"
           :production-tile="boardStore.clickedProductionTile"
@@ -86,5 +84,3 @@ export default {
     </div>
   </div>
 </template>
-
-
